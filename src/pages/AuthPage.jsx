5927f4bb9a7eba9f3b4e26383b79fd3e9dbd0c3a
Katriceb <birch5trice@gmail.com>
2024-01-29 14:19:08 -0500

    first commit

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -5,7 +5,8 @@ import LoginForm from '../components/LoginForm'
 function AuthPage(props) {
   return (
     <>
-    <h1>AuthPage</h1>
+  
+    <h1><div>SaFe Haven Resources </div></h1>
       <SignUpForm setUser={props.setUser}/>
       <LoginForm setUser={props.setUser} />
     </>
